9797bb23ead2727169c77bdffdd8564dd921ed52
Tapas Singhal <98687345+Shocker-lov-t@users.noreply.github.com>
2023-05-24 15:40:37 +0530

    Delete AWS and Django/ec2django directory

--- AWS and Django/ec2django/ec2d1/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-# Create your views here.
-def index(request):
-    return HttpResponse("This is my Django Application")
-def index2(request):
-    return HttpResponse("This IS 2nd Page Of My Django Application")
