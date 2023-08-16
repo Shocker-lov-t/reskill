f227ea202245ae3ead59159beb31963ea1d1b685
Tapas Singhal <98687345+Shocker-lov-t@users.noreply.github.com>
2023-08-16 15:54:33 +0530

    Delete AWS and Django/Demo directory

--- AWS and Django/Demo/Demo/views.py
+++
@@ -1,4 +0,0 @@
-from django.http import HttpResponse
-# Create your views here.
-def index(request):
-    return HttpResponse("AWS Community Builder <br><Strong><Font size='6'>TAPAS SINGHAL</font><strong>")
